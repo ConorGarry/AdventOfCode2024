d9696219beff7c492c3120a78618191a54f6d003
Conor Garry <conorgarry@gmail.com>
2024-12-02 12:57:52 +0000

    Complete Day 02

---
+++ src/Day02.kt
@@ -0,0 +1,33 @@
+import kotlin.math.abs
+
+class Day02 {
+
+    fun part1(input: List<String>): Int =
+        input.toLevelRows().count(::isLevelRowSafe)
+
+    fun part2(input: List<String>): Int =
+        input.toLevelRows().count { (isLevelRowSafe(it) || isLevelFixable(it)) }
+
+    private fun isLevelFixable(levels: List<Int>): Boolean {
+        levels.foldRightIndexed(levels) { index, _, acc ->
+            if (isLevelRowSafe(acc.filterIndexed { idx, _ -> idx != index })) return true
+            acc
+        }
+        return false
+    }
+
+    fun isLevelRowSafe(levels: List<Int>): Boolean =
+        ((levels.zipWithNext().all { (a, b) -> a < b } ||
+                levels.zipWithNext().all { (a, b) -> a > b })) &&
+                levels.zipWithNext().all { (a, b) -> abs(b - a) in 1..3 }
+
+    private fun List<String>.toLevelRows(): List<List<Int>> =
+        map { it.trim().split(" ").map(String::toInt) }
+}
+
+fun main() {
+    with(Day02()) {
+        part1(readInput("Day01")).println() // 624
+        part2(readInput("Day02")).println()
+    }
+}
